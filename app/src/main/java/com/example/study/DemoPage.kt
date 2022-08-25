2bea8f2f7c474449014e9f16b580a95558a3b49f
Efe Kerem Kesgin <appsamurai@Efe-Kerem-Kesgins-Macbook-Pro.local>
2022-08-26 02:29:07 +0300

    add recyclerView & recyclerView-item xmls

--- app/src/main/java/com/example/study/DemoPage.kt
+++ app/src/main/java/com/example/study/DemoPage.kt
@@ -3,9 +3,9 @@ package com.example.study
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class DemoPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.demo_list_view)
     }
 }
